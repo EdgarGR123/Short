ad9f4ad1e3094e64677e200c206c85d196f67159
EdgarGR123 <edgarnahumgarciarosales910@gmail.com>
2025-06-06 16:30:24 -0600

    Update +page.svelte

--- frontend/src/routes/pt/about/+page.svelte
+++ frontend/src/routes/pt/about/+page.svelte
@@ -8,7 +8,7 @@
   <main>
     <h1 class="about-title">Sobre Este Projeto</h1>
     <p class="about-text">Esta ferramenta foi criada para ajudar os usuários a encurtar links e gerar códigos QR de forma fácil e rápida.</p>
-    <p class="about-text">Feita por um desenvolvedor apaixonado por acessibilidade e desempenho.</p>
+    
     <p class="about-text">Inclui recursos práticos como copiar o link encurtado para a área de transferência, baixar a imagem do código QR e abrir o link encurtado em uma nova aba — tudo com apenas um clique.</p>
   </main>
 
